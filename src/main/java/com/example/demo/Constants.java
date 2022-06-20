d00fb9ddfa7d26131b698f2eafae4fd8cb3fac0e
Chandra <mukesh.chandra@capgemini.com>
2022-06-20 20:18:35 +0530

    changes for 1st commit

---
+++ src/main/java/com/example/demo/Constants.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class Constants {
+
+    public static final String ABSS_FILE_PATH = "xxxxxxxxxxx@ubsgen2storageuk.dfs.core.windows.net/Employees2/";
+    public static final String TABLE_NAME = "/Employees3";
+    public static final String ACCESS_KEY = "xxxxxx";
+
+}
